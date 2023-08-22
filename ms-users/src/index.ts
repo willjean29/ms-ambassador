7236deb79dadc1fcd1f1c1eb1770929f7d2228e3
Jean Osco <willjean29@gmail.com>
2023-08-22 13:35:02 -0500

    feat(ms-users): setup database and docker files

---
+++ ms-users/src/index.ts
@@ -0,0 +1,23 @@
+import { createConnection } from "typeorm";
+import express from 'express';
+import cookieParser from "cookie-parser";
+import cors from 'cors';
+
+createConnection().then(async () => {
+
+
+  const app = express();
+
+  app.use(cookieParser());
+  app.use(express.json());
+  app.use(cors({
+    credentials: true,
+    origin: ['*']
+  }));
+
+  // routes(app);
+
+  app.listen(8000, () => {
+    console.log('ms-users runs on port 8000');
+  });
+});
