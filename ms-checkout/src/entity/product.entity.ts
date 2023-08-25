4150e30c9993509528073b76e2509778cfb2c4f6
Jean Osco <willjean29@gmail.com>
2023-08-24 23:15:17 -0500

    init(ms-checkout): initial setup project

---
+++ ms-checkout/src/entity/product.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    image: string;
+
+    @Column()
+    price: number;
+}
---
+++ ms-checkout/src/index.ts
@@ -0,0 +1,52 @@
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import { createConnection } from "typeorm";
+// import { routes } from "./routes";
+import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
+import { createClient } from "redis";
+import AppError from './errors/AppError';
+
+dotenv.config();
+
+export const client = createClient({
+  url: 'redis://redis:6379'
+});
+
+createConnection().then(async () => {
+  await client.connect();
+
+  const app = express();
+
+  app.use(cookieParser());
+  app.use(express.json());
+  app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:3000', 'http://localhost:4000', 'http://localhost:5000']
+  }));
+
+  // routes(app);
+
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    const error = new AppError(`Not Found - ${req.originalUrl}`, 404);
+    next(error);
+  });
+
+  app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return res.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    res.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  });
+
+  app.listen(8000, () => {
+    console.log('ms-checkout runs port 8000');
+  });
+});
+
