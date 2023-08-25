40e0fb30cb8d33ab395f86006c74f10d7441955c
Jean Osco <willjean29@gmail.com>
2023-08-24 22:20:49 -0500

    feat(ms-ambassador): add kafka error handler

--- ms-admin/src/kafka/consumer.ts
+++ ms-admin/src/kafka/consumer.ts
@@ -1,19 +1,26 @@
-import { createConnection } from "typeorm";
+import { createConnection, getRepository } from "typeorm";
 import { kafka } from "./config";
 import { Subscriber } from "./subscriber";
+import { KafkaError } from "../entity/kafka-error.entity";
 
 const consumer = kafka.consumer({ groupId: 'ambassador-consumer' });
 
 createConnection().then(async () => {
   await consumer.connect()
   await consumer.subscribe({ topic: 'admin_topic', fromBeginning: true })
-
+  console.log("ms admin consumer connected");
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
       const key = message.key.toString();
       const value = JSON.parse(message.value.toString());
-      console.log({ key, value })
-      await Subscriber[key](value);
+      try {
+        console.log({ key, value });
+        await Subscriber[key](value);
+      } catch (error) {
+        await getRepository(KafkaError).save({
+          key, value, error: error.message
+        });
+      }
     },
   })
 })
--- ms-ambassador/src/kafka/subscriber.ts
+++ ms-ambassador/src/kafka/subscriber.ts
@@ -1,14 +1,21 @@
 import { getRepository } from "typeorm";
 import { Product } from "../entity/product.entity";
+import { client } from "../index";
 
 export class Subscriber {
   static async productCreated(product: Product) {
     await getRepository(Product).save(product);
+    await client.del('products_frontend')
+    await client.del('products_backend')
   }
   static async productUpdated(product: Product) {
     await getRepository(Product).save(product);
+    await client.del('products_frontend')
+    await client.del('products_backend')
   }
   static async productDeleted(id: number) {
     await getRepository(Product).delete(id);
+    await client.del('products_frontend')
+    await client.del('products_backend')
   }
 }
